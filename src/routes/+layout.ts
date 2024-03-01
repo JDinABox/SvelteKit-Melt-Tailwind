360e3f99d9194dc63b983146879c67475dbf6e2a
JD <37222721+JDinABox@users.noreply.github.com>
2024-03-01 23:23:18 +0000

    Initial commit

---
+++ src/routes/+layout.ts
@@ -0,0 +1,15 @@
+import '../app.css';
+
+import { dark } from '$stores/theme';
+
+import { browser } from '$app/environment';
+
+if (browser) {
+	dark.subscribe((value) => {
+		if (value) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	});
+}
